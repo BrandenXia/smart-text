c12ae1932ae339baff5d3828a72a66ceb6a20a48
BrandenXia <xxtbranden@outlook.com>
2024-03-29 23:42:01 -0700

    feat: add styling for heading and list

--- src/components/Editor/theme.ts
+++ src/components/Editor/theme.ts
@@ -1,6 +1,18 @@
 import { EditorThemeClasses } from "lexical";
 
 const theme = {
+  heading: {
+    h1: "text-4xl font-bold",
+    h2: "text-3xl font-bold",
+    h3: "text-2xl font-bold",
+    h4: "text-xl font-bold",
+    h5: "text-lg font-bold",
+    h6: "text-base font-bold",
+  },
+  list: {
+    ul: "list-inside list-disc",
+    ol: "list-inside list-decimal",
+  },
   text: {
     bold: "font-bold",
     italic: "italic",
